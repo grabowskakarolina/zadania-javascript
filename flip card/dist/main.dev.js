e6b10dff27744fd6391593a021b13f32dfadb5af
Karolina Grabowska <53982166+grabowskakarolina@users.noreply.github.com>
2020-08-26 20:39:40 +0200

    .

---
+++ flip card/dist/main.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".flipper").on("click", function () {
+    $(this).find(".flipper-card").toggleClass("flipper-is-flipped");
+  });
+});
--- flip card/main.js
+++ flip card/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-
-    $('.flipper').on('click', function() {
-        $(this).find('.flipper-card').toggleClass('flipper-is-flipped');
+    $(".flipper").on("click", function() {
+        $(this).find(".flipper-card").toggleClass("flipper-is-flipped");
     });
-
 });
